99ef3f643a43fc96f823c98013f68ff81ab6f270
seniorman-dev <coder.jay2001@gmail.com>
2025-11-29 14:05:47 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -125,7 +125,6 @@ app = create_app()
 
 
 # Local development entry point
-'''if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-    #app.run(debug=True)'''
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # use Railway port OR default 5000 locally
+    app.run(host="0.0.0.0", port=port, debug=True)
