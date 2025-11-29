1c1c07e6f63159b1bcfea23d63d07bff1e054169
seniorman-dev <coder.jay2001@gmail.com>
2025-11-29 23:17:35 +0000

    modified:   utils/email_service.py

--- utils/email_service.py
+++ utils/email_service.py
@@ -4,7 +4,8 @@ from extensions import mail
 import threading
 from flask import copy_current_request_context
 #import resend
-
+import requests
+import os
 
 
 
@@ -34,7 +35,7 @@ def send_email(sender: str, recipient: str, subject: str,  content: str):
 def async_send_global_email(sender: str, recipient: str, subject: str,  content: str):
     @copy_current_request_context
     def _send():
-        brevo_mail(sender=sender, recipient=recipient, subject=subject, content=content)
+        send_brevo_email(sender=sender, recipient=recipient, subject=subject, content=content)
         #send_email(sender=sender, recipient=recipient, subject=subject, content=content)
 
     thread = threading.Thread(target=_send)
@@ -42,7 +43,7 @@ def async_send_global_email(sender: str, recipient: str, subject: str,  content:
     
     
 
-def brevo_mail(sender: str, recipient: str, subject: str,  content: str) -> str:
+'''def brevo_mail(sender: str, recipient: str, subject: str,  content: str) -> str:
     
     msg = Message(subject=subject, body=content, recipients=[recipient],sender=sender)
     
@@ -52,4 +53,34 @@ def brevo_mail(sender: str, recipient: str, subject: str,  content: str) -> str:
         return 'Email sent successfully!'
     except Exception as e:
         print(f'Error sending mail with Brevo: {str(e)}')
-        return f'Error sending mail with Brevo: {str(e)}'
+        return f'Error sending mail with Brevo: {str(e)}'''
+    
+    
+
+
+
+def send_brevo_email(recipient: str, subject: str, content: str, sender: str, sender_name: str = "Your App"):
+    """Send email via Brevo API"""
+    
+    url = "https://api.brevo.com/v3/emailCampaigns"
+    
+    headers = {
+        "api-key": "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",
+        "content-type": "application/json"
+    }
+    
+    payload = {
+        "sender": {"name": sender_name, "email": sender},
+        "to": [{"email": recipient}],
+        "subject": subject,
+        "htmlContent": content
+    }
+    
+    response = requests.post(url, json=payload, headers=headers)
+    
+    if response.status_code == 201:
+        print(f"Email sent!: {response.json()}")  #response.json().get('messageId')
+        return True
+    else:
+        print(f"Error: {response.json()}")
+        return False
