371149cfd4516d723c2ada256a6e04f7d1b39544
seniorman-dev <coder.jay2001@gmail.com>
2025-11-29 14:03:18 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ app = create_app()
 
 
 # Local development entry point
-if __name__ == "__main__":
+'''if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-    #app.run(debug=True)
+    #app.run(debug=True)'''
